7bf095bdf2b15123d0b058477f2439ee2d9d660a
Valera <khvalera@narod.ru>
2013-08-29 16:35:19 +0300

    Update test-inet.sh

--- test-inet.sh
+++ test-inet.sh
@@ -1,28 +1,48 @@
 #!/bin/bash
 IP=$1
 st=0
-logfile=$2
+path_log=$2
+cycle=$3
+
 if ! [ ${IP} ]; then
  IP=8.8.8.8
 fi
-while true; do
+
+if ! [ ${cycle} ]; then
+ cycle=0
+fi
+
+cycle_ping() {
   res=1
   /bin/ping ${IP} -c 2 > /dev/null 2>&1 && res=0
   datal=`date +%Y%m%d`
-  if ! [ ${logfile} ]; then
-    logfile="/var/log/test-ip-${datal}-${IP}.log"
-  fi
   datat=`date +%T`
   if [ ${res} -eq 0 ]; then
     if [ ${st} = 0 ]; then
-      echo "${datat} - internet yes" 2>&1 | tee -a ${logfile}
-      st=1
+       if [ ${path_log} ]; then
+         echo "${datat} - internet yes" 2>&1 | tee -a ${path_log}"/test-ip-${datal}-${IP}.log"
+       else
+         echo "${datat} - internet yes"
+       fi
+       st=1
     fi
   else
     if [ ${st} = 1 ]; then
-      echo "${datat} - internet no" 2>&1 | tee -a ${logfile}
+      if [ ${path_log} ]; then
+        echo "${datat} - internet no" 2>&1 | tee -a ${path_log}"/test-ip-${datal}-${IP}.log"
+      else
+        echo "${datat} - internet no"
+      fi
       st=0
     fi
   fi
   sleep 2
-done
+}
+
+if [ ${cycle} = 1 ]; then
+  while true; do
+    cycle_ping
+  done
+else
+   cycle_ping
+fi
